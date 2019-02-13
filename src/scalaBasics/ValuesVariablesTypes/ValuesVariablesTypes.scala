8e902dcb565c50f20fb95b68f585bb71b88cbe24
James Willett <james.willett@sap.com>
2019-02-13 07:15:30 +0000

    Initial commit with first code

---
+++ src/scalaBasics/ValuesVariablesTypes/ValuesVariablesTypes.scala
@@ -0,0 +1,20 @@
+package scalaBasics.ValuesVariablesTypes
+
+object ValuesVariablesTypes extends App {
+
+  val x = 42
+  println(x)
+
+  val myString: String = "James"
+  val myBoolean: Boolean = true
+  val myChar: Char = 'd'
+  val myInt: Int = 33
+  val myShort: Short = 326
+  val myLong: Long = 35263256234L // notice the L on the end - this tells the compiler its a Long
+  val myFloat: Float = 3.0f // again notice the f on the end
+  val myDouble: Double = 4.27
+
+  var myVariable: Int = 27
+  myVariable = 6
+
+}
