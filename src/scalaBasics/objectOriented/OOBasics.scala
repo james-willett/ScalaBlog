46feedf3342c4d1744d4e3b3e2b94ea1c1551c1a
James Willett <james.willett@sap.com>
2019-06-26 06:38:39 +0100

    Adding exercises code to OO Basics

--- src/scalaBasics/objectOriented/OOBasics.scala
+++ src/scalaBasics/objectOriented/OOBasics.scala
@@ -19,6 +19,29 @@ object OOBasics extends App {
   person4.greet2("Andrew")
 
   person4.greet()
+
+  // EXERCISE 1 - NOVEL and AUTHOR
+
+  val author = new Writer("Jimmy", "Stevens", 1967)
+  val novel = new Novel("My Great Book", 1988, author)
+
+  println(novel.authorAge)
+
+  println(novel.isWrittenBy(author))
+
+  val imposter = new Writer("Jimmy", "Stevens", 1967)
+  println(novel.isWrittenBy(imposter))
+
+  val copyOfNovel: Novel = novel.copy(2001)
+
+  // EXERCISE 2 - COUNTER CLASS
+  val counter = new Counter(0) // replace with default
+
+  counter.inc.print
+
+  counter.inc.inc.inc.print
+
+  counter.inc2(10).print
 }
 
 class Person
@@ -42,5 +65,43 @@ class Person4(name: String, val age: Int) {
   def this(name: String) = this(name, 0)
 
   def this() = this("Chris")
+}
 
+// EXERCISE 1 - NOVEL and AUTHOR
+
+class Writer(firstName: String, surname: String, val year: Int) {
+
+  def fullName: String = firstName + " " + surname
+}
+
+class Novel(name: String, year: Int, author: Writer) {
+
+  def authorAge = year - author.year
+
+  def isWrittenBy(author: Writer) = author == this.author
+
+  def copy(newYear: Int): Novel = new Novel(name, newYear, author)
+}
+
+// EXERCISE 2 - COUNTER CLASS
+
+class Counter(val count: Int) {
+
+  def inc = new Counter(count + 1)
+  def dec = new Counter(count - 1)
+
+  def inc(n: Int) = new Counter(count + n)
+  def dec(n: Int) = new Counter(count - n)
+
+  def inc2 = new Counter(count + 1) {
+    println("incrementing")
+    new Counter(count + 1)
+  }
+
+  def inc2(n: Int): Counter = {
+    if (n <= 0) this
+    else inc2.inc2(n-1)
+  }
+
+  def print = println(count)
 }
